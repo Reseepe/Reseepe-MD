41b5ee43cf29c3dd0fdc439c869e7bb0aa1278f7
ahmad.fatahillah <ahmadfatahillah16@gmail.com>
2024-06-08 18:17:10 +0700

    feat: add bottom navigation

--- app/src/main/java/com/capstone/reseepe/ui/main/MainActivity.kt
+++ app/src/main/java/com/capstone/reseepe/ui/main/MainActivity.kt
@@ -10,10 +10,15 @@ import android.view.View
 import android.view.WindowInsets
 import android.view.WindowManager
 import androidx.activity.viewModels
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.capstone.reseepe.R
 import com.capstone.reseepe.databinding.ActivityMainBinding
 import com.capstone.reseepe.ui.welcome.WelcomeActivity
 import com.capstone.reseepe.util.ViewModelFactory
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
@@ -34,44 +39,59 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        setupView()
-        setupAction()
-        playAnimation()
-    }
+        val navView: BottomNavigationView = binding.navView
 
-    private fun setupView() {
-        @Suppress("DEPRECATION")
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            window.insetsController?.hide(WindowInsets.Type.statusBars())
-        } else {
-            window.setFlags(
-                WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home,
+                R.id.navigation_scan,
+                R.id.navigation_bookmarks,
+                R.id.navigation_profile
             )
-        }
-        supportActionBar?.hide()
+        )
+        navView.setupWithNavController(navController)
+
+//        setupView()
+//        setupAction()
+//        playAnimation()
     }
 
-    private fun setupAction() {
-        binding.logoutButton.setOnClickListener {
-            viewModel.logout()
-        }
-    }
-
-    private fun playAnimation() {
-        ObjectAnimator.ofFloat(binding.imageView, View.TRANSLATION_X, -30f, 30f).apply {
-            duration = 6000
-            repeatCount = ObjectAnimator.INFINITE
-            repeatMode = ObjectAnimator.REVERSE
-        }.start()
-
-        val name = ObjectAnimator.ofFloat(binding.nameTextView, View.ALPHA, 1f).setDuration(100)
-        val message = ObjectAnimator.ofFloat(binding.messageTextView, View.ALPHA, 1f).setDuration(100)
-        val logout = ObjectAnimator.ofFloat(binding.logoutButton, View.ALPHA, 1f).setDuration(100)
-
-        AnimatorSet().apply {
-            playSequentially(name, message, logout)
-            startDelay = 100
-        }.start()
-    }
+//    private fun setupView() {
+//        @Suppress("DEPRECATION")
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+//            window.insetsController?.hide(WindowInsets.Type.statusBars())
+//        } else {
+//            window.setFlags(
+//                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//                WindowManager.LayoutParams.FLAG_FULLSCREEN
+//            )
+//        }
+//        supportActionBar?.hide()
+//    }
+//
+//    private fun setupAction() {
+//        binding.logoutButton.setOnClickListener {
+//            viewModel.logout()
+//        }
+//    }
+//
+//    private fun playAnimation() {
+//        ObjectAnimator.ofFloat(binding.imageView, View.TRANSLATION_X, -30f, 30f).apply {
+//            duration = 6000
+//            repeatCount = ObjectAnimator.INFINITE
+//            repeatMode = ObjectAnimator.REVERSE
+//        }.start()
+//
+//        val name = ObjectAnimator.ofFloat(binding.nameTextView, View.ALPHA, 1f).setDuration(100)
+//        val message = ObjectAnimator.ofFloat(binding.messageTextView, View.ALPHA, 1f).setDuration(100)
+//        val logout = ObjectAnimator.ofFloat(binding.logoutButton, View.ALPHA, 1f).setDuration(100)
+//
+//        AnimatorSet().apply {
+//            playSequentially(name, message, logout)
+//            startDelay = 100
+//        }.start()
+//    }
 }
---
+++ app/src/main/java/com/capstone/reseepe/ui/scan/ScanFragment.kt
@@ -0,0 +1,46 @@
+package com.capstone.reseepe.ui.scan
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.capstone.reseepe.R
+import com.capstone.reseepe.databinding.FragmentBookmarksBinding
+import com.capstone.reseepe.databinding.FragmentScanBinding
+import com.capstone.reseepe.ui.bookmarks.BookmarksViewModel
+
+class ScanFragment : Fragment() {
+
+    private var _binding: FragmentScanBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val scanViewModel =
+            ViewModelProvider(this).get(ScanViewModel::class.java)
+
+        _binding = FragmentScanBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textHome
+        scanViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
